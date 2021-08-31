6a86620accb44daf6a11633a9e2212d8e30a52c4
xXYo-NachoXx <nacho4real@estudiantec.cr>
2021-08-31 00:35:05 -0600

    Updates

---
+++ precache-manifest.b91cd429c22d89b2edac0c5b040417fb.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e82fe049f4f8f668fedc401d0d6912",
+    "url": "/carlosfolio/index.html"
+  },
+  {
+    "revision": "59db4bc5318d1aeea5fb",
+    "url": "/carlosfolio/static/css/main.9d01557d.chunk.css"
+  },
+  {
+    "revision": "dfe51178ef17cbbcb071",
+    "url": "/carlosfolio/static/js/2.47712d1f.chunk.js"
+  },
+  {
+    "revision": "59db4bc5318d1aeea5fb",
+    "url": "/carlosfolio/static/js/main.9bb15ad3.chunk.js"
+  },
+  {
+    "revision": "62447f90ffb5240591b3",
+    "url": "/carlosfolio/static/js/runtime-main.ad9bf133.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/carlosfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "b8349593342654d8ca5686e6cb73f13b",
+    "url": "/carlosfolio/static/media/Code.b8349593.ttf"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/carlosfolio/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/carlosfolio/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/carlosfolio/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/carlosfolio/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/carlosfolio/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/carlosfolio/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/carlosfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "a585e50f6a3bbb9a5e2dd337a23621f7",
+    "url": "/carlosfolio/static/media/data.a585e50f.jpg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/carlosfolio/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/carlosfolio/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "9d204f50ec8e5bcc5dcda0132d404b48",
+    "url": "/carlosfolio/static/media/fa-brands-400.9d204f50.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/carlosfolio/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/carlosfolio/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/carlosfolio/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "9443c1e7fdf95d739af3f0901224f90a",
+    "url": "/carlosfolio/static/media/fa-regular-400.9443c1e7.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/carlosfolio/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/carlosfolio/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/carlosfolio/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/carlosfolio/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/carlosfolio/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "3d102342391af184d5ae9e7708d8220f",
+    "url": "/carlosfolio/static/media/fa-solid-900.3d102342.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/carlosfolio/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/carlosfolio/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "2192860b1cdc5eb2b907cd788f485380",
+    "url": "/carlosfolio/static/media/logo-01.2192860b.png"
+  },
+  {
+    "revision": "1a4289f588b997060f984421017aed1e",
+    "url": "/carlosfolio/static/media/profile.1a4289f5.jpg"
+  },
+  {
+    "revision": "9331f9728bff50b3d6b330f7d5d6bc51",
+    "url": "/carlosfolio/static/media/web.9331f972.svg"
+  }
+]);
